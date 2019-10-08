0861dcb4a20f167193b750135c32f3a3cc993fb7
marcreativo <marcreativo@gmail.com>
2019-10-08 20:26:07 -0300

    todo list in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { CssBaseline, Container } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	state = {
+		task: '',
+		todo: []
+	}
+
+	fieldHandler(e) {
+		this.setState({ task: e.target.value })
+	}
+
+	enterHandler(e) {
+		if (e.key === 'Enter') {
+			let value = this.state[e.target.name]
+			let newTodo = [ ...this.state.todo, value ]
+			this.setState({ task: '', todo: newTodo })
+		}
+	}
+
+	render() {
+		return (
+			<Container>
+				<CssBaseline />
+				<h1>Todo</h1>
+				<TextField
+					value={this.state.task}
+					label={'Ingrese tarea'}
+					name={'task'}
+					onChange={(e) => this.fieldHandler(e)}
+					onKeyPress={(e) => this.enterHandler(e)}
+					variant="outlined"
+				/>
+			</Container>
+		)
+	}
 }
 
-export default App;
+export default App
